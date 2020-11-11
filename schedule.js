c2bb6eae6cd2a047246aad9f1515e7261733ebb6
Trevor <tlow94@uw.edu>
2020-11-11 11:56:31 -0800

    quickSort

--- schedule.js
+++ schedule.js
@@ -1,18 +1,17 @@
 //Today
-    //Infosys Interview Study Guide
     //1 new sort
-    //medium article
-    //Interview 3:30
-    //Redux Finish + Ecommerce start
-    //Leet code 
+    //Redux Selectors
+    //Prep for startUp interview
+
 
 //Tomorrow
-    //Interview Review
-    //10:30 infosys review
-    //Redux Selectors
-    //Ecommerce Continue
-    //1 new sort/ rev merge sort
+    //Redux E-commerce Start
+    //1 new sort 1 old sort
     //leet code
+
+   
+
+
 
 
 
