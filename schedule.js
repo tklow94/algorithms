e986b955d44be5a4a62bdf25a59d64e4be8eb7ee
Trevor <tlow94@uw.edu>
2020-11-13 12:55:04 -0800

    leetcode 1

--- schedule.js
+++ schedule.js
@@ -1,21 +1,9 @@
-//Today
-    //Redux E-commerce 
-    //Fix Project description plus video plus resume
-    //1 outreach attempt
-    //Talk with Jim Lau
+//1 leet code
+//Networking 2 contacts
+//E-commerce
 
 
-//Tomorrow
-    //All sort Rev
-    //2 reach outs
-    //redux E-commerce
 
-
-//Sat 
-    //two reach outs
-
-//Sun
-    //two reach outs
     
 
 
