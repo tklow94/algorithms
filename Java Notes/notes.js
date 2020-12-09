b19a9b38edaabb00655ad404d3b85e76ebdfdd82
Trevor <tlow94@uw.edu>
2020-12-09 12:05:28 -0800

    leetcode

---
+++ Java Notes/notes.js
@@ -0,0 +1,19 @@
+//Java is case sensitive
+
+//public is an access modifier, which tells us what kind of scope access we have. public gives full access.
+
+//class defines a class, followed by the declared class name and the class body {}.
+
+//to define a class, you need an access modifier like public that comes before it.
+
+    //CARD: make a flashcard that makes you type the components that make up a class declaration, and explain what each one does.
+
+
+
+
+
+
+//method is a collection of statements that perform an operation. A special method, the main method is needed in all java code. It is what Java looks for when running a program and is the entry point for code.
+    //public is the same here, static we need, void tells us the method will not return anything. () are required, and can include one or more parameters.
+    //We added a statement to our method body, it needs a ; at the end, and the statement is a command to be executed.
+        //
