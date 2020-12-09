12e11bfca1412c24bc65df9b3f09c24d168c1a49
Trevor <tlow94@uw.edu>
2020-12-09 13:15:03 -0800

    data

---
+++ DataStructures/HashTables/hashFunctionIntro.js
@@ -0,0 +1,10 @@
+//converts keys into valid array indices.
+//allows us to pass in data of any size, and receive an output of a fixed size, meaning the size of the data output no matter the size of the input is the same.
+    //"hello" => 12
+    //"Trevor is the best" => 13
+
+//a good hash function is fast, constant time O(1)
+//Distributes keys uniformly, meaning random assignment amd/or evenly spread.
+//Same input results in the same output everytime. It is deterministic.
+
+
---
+++ DataStructures/HashTables/hashTableIntro.js
@@ -0,0 +1,11 @@
+//Hash table is used to store key value pairs, but not ordered. Fast for finding, adding, removing values. Don't have to search through the entire data set to do these compare to an array.
+
+
+//take an input key and convert it into a valid array indices. This is how we are able to lookup values by key if we have an index assigned to that key.
+    //hash functions help us do this.
+    //EX: at index 0, we could have ["pink", "#ff69b4"]
+
+
+
+
+
