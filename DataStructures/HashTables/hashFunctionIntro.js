e9ccd421e948f7178823a85de974fefe81f4830c
Trevor <tlow94@uw.edu>
2020-12-10 17:36:10 -0800

    hellomoto

--- DataStructures/HashTables/hashFunctionIntro.js
+++ DataStructures/HashTables/hashFunctionIntro.js
@@ -3,11 +3,55 @@
     //"hello" => 12
     //"Trevor is the best" => 13
 
-    
+
 //a good hash function is fast, constant time O(1)
 
 //Distributes keys uniformly, meaning random assignment amd/or evenly spread. Doesn't return the same output for different inputs.
 
 //Same input results in the same output everytime. It is deterministic.
+
+
+//every character has a numeric value associated with it accesssed with 97, and if you subtract 96 from the charCodeAt then we get the position in the alphabet.
+
+//though process is to add up all the character codes unicodes to get the unique numerical identifier for that string.
+
+
+//for hash that works on strings only!
+//Not constant Time, it is linear!
+// Not very random, data is highly clustered.
+
+function Hash(key, arrayLen){
+    let total = 0;
+    for(let char of key){
+        // map "a" to 1, "b" to 2, etc.
+        let value = char.charCodeAt(0) - 96
+        total = (total + value) % arrayLen;
+    }
+    return total;
+}
+
+//returns an index that fits into our arrayLen that is a constraint.
+
+
+
+// lets try to increase to constant time and increase the randomness.
+
+//going to be using prime numbners to increase the randomness
+//going to be adding a Math.min(key.length, 100) so that our loop only goes through a constant number of times.
+
+function Hash2(key, arrayLen){
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for(let i = 0; i< Math.min(key.length, 100); i++){
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        total = (total * WEIRD_PRIME + value) % arrayLen;
+    }
+    return total;
+}
+//Prime number for a very complex reason improves the randomness.
+//Math.min constraint allows for constant time.
+
+
 
 
---
+++ LeetCode/containsDuplicate.js
@@ -0,0 +1,23 @@
+//Given an array of integers, find if the array contains any duplicates.
+
+//Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+//create a set and loop the array into the set.
+    //if the set has less length than the array, it contains duplicates.
+
+
+    function containsDuplicate (arr) {
+        let set = new Set;
+        for(let i = 0; i<arr.length; i++){
+            set.add(arr[i])
+        }
+        if(set.size !== arr.length){
+            return true
+        }
+        return false
+    }
+    
+    console.log(containsDuplicate([1,2,3,1]))
+    
+    //time is O(n)
+    //space is O(n)
