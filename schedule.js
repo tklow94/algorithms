5a79e4e356d24dba5f2d5cca903660dd91914f10
Trevor <tlow94@uw.edu>
2020-12-15 13:44:00 -0800

    leet

--- schedule.js
+++ schedule.js
@@ -1,7 +1,7 @@
 //Ruby  8 hrs/week (2) 116min
 //React 10 hrs/week (2)
 //Java 8hrs/week (2)
-//leet code 1 & 1/2 lesson Alternate between leet and hackerrank.
+//leet code 1 & 1/2 lesson 
 //Medium Article (1/week)
 
 
@@ -12,8 +12,6 @@
 
 //Today:
     //ruby time
-    //hacker rank
-        //medium
     //ruby time (hopefully finish if not saturday baby)
 
 
