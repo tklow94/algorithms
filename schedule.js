f39d43cc2bce70c4d796f0b8b6d37f857b2649bb
Trevor <tlow94@uw.edu>
2020-12-16 12:17:21 -0800

    leet

--- schedule.js
+++ schedule.js
@@ -1,18 +1,24 @@
-//Ruby  8 hrs/week (2) 116min
-//React 10 hrs/week (2)
+//Ruby  8 hrs/week (2) 
 //Java 8hrs/week (2)
 //leet code 1 & 1/2 lesson 
 //Medium Article (1/week)
 
 
 
-//(might take 1-2 days per cycle)
+
 
 
 
 //Today:
+    //leet 
+    //intern apply
     //ruby time
-    //ruby time (hopefully finish if not saturday baby)
+
+    //java time
+    //ruby time
+    //Dinner
+
+ 
 
 
 
