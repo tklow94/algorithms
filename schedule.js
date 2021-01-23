04eb682484d98af739d15c7f491c4314c15ddefd
Trevor <tlow94@uw.edu>
2021-01-22 22:39:55 -0800

    algo

--- schedule.js
+++ schedule.js
@@ -1,26 +1,9 @@
-//Ruby  8 hrs/week (2) 
-//Java 8hrs/week (2)
-//leet code 1 & 1/2 lesson 
-//Medium Article (1/week)
 
-
-
-
-
-
-
-//Today:
-    //leet 
-    //intern apply
-    //ruby time
-
-    //java time
-    //ruby time
-    //Dinner
+   
 
  
 
-
+ 
 
 
 
