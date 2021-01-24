1f97aae2a106a11f77f54c294f043cbd4f778273
Trevor <tlow94@uw.edu>
2021-01-23 21:30:34 -0800

    valleys

--- test.js
+++ test.js
@@ -1,30 +1,19 @@
-
-
-function sockMerchant(n,ar) {
- 
-
-  let hash = {}
-  
-  for(i = 0; i<n; i++ ) {
-    if(!hash[ar[i]]){
-        hash[ar[i]] = 1;
-    }else{
-        hash[ar[i]] += 1
+function countingValleys(steps, path) {
+    ar = path.split("")
+    let count = 0;
+    let valleys = 0;
+    for(let i = 0; i<steps; i++) {
+        if(path[i] === "U"){
+            count++;
+        }else{
+            count--;
+        }
+        if(count === 0 && path[i] === "U"){
+            valleys++
+        }
     }
-  }
+    return valleys;
 
-  let pairs = n;
-
-  for(j=0; j<Object.keys(hash).length; j++){
-    if(Object.values(hash)[j] % 2 != 0){
-        pairs--
-    }
-  }
-  
-  return Math.floor(pairs/2)
-   
 }
 
-console.log(sockMerchant(9,[10,20,20,10,10,30,50,10,20]))
-
-
+console.log(countingValleys(8,'UDDDUDUU'))
