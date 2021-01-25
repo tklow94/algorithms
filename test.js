2b03fe546b98bdef0fd578cc43538540587de76f
Trevor <tlow94@uw.edu>
2021-01-24 19:52:56 -0800

    clouds

--- test.js
+++ test.js
@@ -1,19 +1,33 @@
-function countingValleys(steps, path) {
-    ar = path.split("")
-    let count = 0;
-    let valleys = 0;
-    for(let i = 0; i<steps; i++) {
-        if(path[i] === "U"){
-            count++;
-        }else{
-            count--;
-        }
-        if(count === 0 && path[i] === "U"){
-            valleys++
+//jump on anyt cloud +1 or +2
+//only clouds labelded 1 are bad, 0 are good
+//alays possible to win
+//return minimum number of jumps
+
+
+function clouds(ar) {
+    let i = 0;
+    let j = 1
+    let jumps = 0;
+    while(j<ar.length){
+        if(ar[j] != 0){
+            i += 2;
+            j += 2;
+            jumps++;
+        }else {
+           if(ar[j+1]==0){
+            i += 2;
+            j += 2;
+            jumps++;
+           }else {
+               i++;
+               j++;
+               jumps++
+           }
         }
     }
-    return valleys;
+    return jumps
 
 }
 
-console.log(countingValleys(8,'UDDDUDUU'))
+console.log(clouds([0,1,0,0,0,1,0])) //should be 3 jumps
+                            
