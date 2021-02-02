3dcb3fdd645dde2c1c21bb00d91245bbd523da27
Trevor <tlow94@uw.edu>
2021-02-01 22:15:50 -0800

    dll

--- tester.js
+++ tester.js
@@ -41,6 +41,18 @@ class DLL {
         this.length--;
         return this;
     }
+    unshift(val){
+        let newNode =new Node(val);
+        if(!this.head === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        this.head.prev = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++
+        return list
+    }
 
 }
 
